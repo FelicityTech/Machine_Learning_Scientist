474f72185aa912287e5e9de2a1e249a5e209f67c
FelicityTech <souceking@gmail.com>
2024-11-02 18:06:46 +0100

    probability distribution

---
+++ Statistics in python/prob_dist.py
@@ -0,0 +1,6 @@
+# Create probability distribution
+size_dist = restaurant_groups['group_size'].value_counts() / restaurant_groups.shape[0]
+# Reset index and rename columns
+size_dist = size_dist.reset_index()
+size_dist.columns = ['group_size', 'prob']
+print(size_dist)
